aa0c6be736eaefb5faefe871d8afe1d60fa0ec13
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-01-18 22:59:38 -0800

    Update python parser to understand private references and deeply nested attributes

--- run.py
+++ run.py
@@ -22,48 +22,92 @@ STANDARD_LIBRARY = stdlib_list('2.7')
 
 
 class ReferenceCollector(ast.NodeVisitor):
+    # see https://greentreesnakes.readthedocs.io/en/latest/nodes.html for good reference
 
-    def __init__(self):
+    def __init__(self, private_namespace):
         super(ReferenceCollector, self).__init__()
         self.bindings = dict()
         self.use_count = Counter()
+        self.bindings.update({ name: '__private__.' + name for name in private_namespace })
+        self.bindings.update({ name: '__stdlib__.' + name for name in STANDARD_LIBRARY })
+
+    def add_grammar(self, node):
+        self.use_count.update(['__stdlib__.' + node.__class__.__name__])
 
     def visit(self, node):
         super(ReferenceCollector, self).visit(node)
-        self.use_count.update(['__stdlib__.' + node.__class__.__name__])
-        return self.use_count
 
-    def noop(self):
-        return self.use_count
+    def generic_visit(self, node):
+        self.add_grammar(node)
+        super(ReferenceCollector, self).generic_visit(node)
 
     def add_binding(self, bound_name, real_name):
+        if bound_name in self.bindings:
+            return
         if real_name.split('.')[0] in STANDARD_LIBRARY:
             self.bindings[bound_name] = '__stdlib__.' + real_name
         else:
             self.bindings[bound_name] = real_name
 
-    def add_use(self, bound_name):
-        return self.use_count.update([ self.bindings[bound_name] ])
+    def add_use(self, *attributes):
+        if attributes[0] not in self.bindings:
+            # we can't know all bindings, because of imports like "from foo import *"
+            # In such a case, we can't really do anything with the reference
+            return
+        real_name = self.bindings[attributes[0]]
+        full_name = '.'.join([real_name] + list(attributes[1:]))
+        self.use_count.update([ full_name ])
 
     def visit_Import(self, node):
+        self.add_grammar(node)
         for alias in node.names:
             self.add_binding(alias.asname or alias.name, alias.name)
-            self.add_use(alias.asname or alias.name)
 
     def visit_ImportFrom(self, node):
+        self.add_grammar(node)
         if node.level == 0:
             for alias in node.names:
-                self.add_binding(alias.asname or alias.name, node.module)
-                self.add_use(alias.asname or alias.name)
+                self.add_binding(alias.asname or alias.name, node.module + '.' + alias.name)
         else:
-            # relative import means private module
+            # Relative import
+            # TODO: Actually add this under __private__ namespace
             pass
 
-    def visit_Name(self, node):
-        if node.id in self.bindings:
-            self.add_use(node.id)
+    def visit_Attribute(self, attribute):
+        name = self.get_name(attribute)
+        self.add_use(*name)
+        self.add_grammar(attribute)
 
+    def get_name(self, node):
+        if isinstance(node, ast.Name):
+            return [ node.id ]
+        elif isinstance(node, ast.Attribute):
+            return self.get_attribute_name(node)
+        elif isinstance(node, ast.Call):
+            return self.get_call_name(node)
+        elif isinstance(node, ast.Subscript):
+            return self.get_name(node.value)
+        else:
+            return []
 
+    def get_attribute_name(self, attribute):
+        self.add_grammar(attribute)
+        attributes = []
+        expression = attribute
+        while isinstance(expression, ast.Attribute):
+            attributes.insert(0, expression.attr)
+            expression = expression.value
+        attributes = self.get_name(expression) + attributes
+        return attributes
+
+    def get_call_name(self, call):
+        self.add_grammar(call)
+        return self.get_name(call.func)
+
+    def visit_Name(self, name):
+        self.add_grammar(name)
+        if name.id in self.bindings:
+            self.add_use(name.id)
 
 def handle_data(sock):
     data_as_str = ''
@@ -80,16 +124,9 @@ def handle_data(sock):
         try:
             code = base64.b64decode(json_object['code'].encode('utf-8'))
             context = json_object['context']
-            if 'path' not in context:
-                LOGGER.warning('No filename provided to parser!')
-                filename = '<unknown>'
-            else:
-                filename = context['path']
-            data = {
-                'use_count': ReferenceCollector().visit(
-                    ast.parse(code, filename = filename)
-                )
-            }
+            reference_collector = ReferenceCollector(context['private_modules'] if 'private_modules' in context else [])
+            reference_collector.visit(ast.parse(code, filename = context['filename'] if 'filename' in context else '<unknown>'))
+            data = { 'use_count': reference_collector.use_count }
         except KeyError as exc:
             data = { 'error': errno.EINVAL, 'message': str(exc) }
         except ValueError as exc:
